2f82546ef1b3ef9a16ade2a38685d47409dea597
David <obohdavid2014@gmail.com>
2022-07-06 14:28:39 -0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef } from 'react';
+import './App.scss';
+import {Canvas, useFrame} from "react-three-fiber"
+
+
+const SpinningMesh = ({position, args, color}) => {
+  const mesh = useRef(null)
+  useFrame(() => {
+    mesh.current.rotation.x = mesh.current.rotation.y += 0.01
+  })
+
+  return (
+    <mesh castShadow position={position} ref={mesh}>
+        <boxBufferGeometry attach='geometry' args={args} />
+        <meshStandardMaterial attach='material' color={color} /> 
+      </mesh>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Canvas shadowMap colormanagement = 'true' camera={{position: [-5, 2, 10], fov: '60'}}>
+      <ambientLight intensity={0.3} />
+      <directionalLight 
+        castShadow 
+        position={[0, 10, 0]}
+        intensity={1}
+        shadow-mapSize-width = {1024}
+        shadow-mapSize-height = {1024}
+        shadow-camera-far = {50}
+        shadow-camera-left = {-10}
+        shadow-camera-right = {10}
+        shadow-camera-top = {50}
+        shadow-camera-bottom = {-10}
+      />
+      <pointLight position={[-10, 0 -20]} intensity={0.5} />
+      <pointLight position={[0, -10, 0]} intensity={1.5} />
+      <group>
+      <mesh
+            rotation={[-Math.PI / 2, 0, 0]}
+            position={[0, -3, 0]}
+            receiveShadow>
+            <planeBufferGeometry attach='geometry' args={[100, 100]} color={'yellow'} />
+            <shadowMaterial attach='material' opacity={0.3} />
+      </mesh>
+      </group>
+
+    <SpinningMesh position={[0, 1, 0]} args={[3, 2, 1]} color='lightblue' /> 
+    <SpinningMesh position={[-2, 1, -5]} color='pink' /> 
+    <SpinningMesh position={[5, 1, -2]} color='pink' />    
+    </Canvas>
+    </>
   );
 }
 
